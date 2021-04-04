00daacad8a7658e799f86ab5cfc2c5e98e091d37
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-04 18:51:21 +0900

    Modify クラス化

--- rakuten_categories.rb
+++ rakuten_categories.rb
@@ -1,14 +1,25 @@
 require 'nokogiri'
 require 'open-uri'
 
-charset = nil
+class RakutenCategory
+  def url
+    "https://search.rakuten.co.jp/search/event/%E3%81%B5%E3%82%8B%E3%81%95%E3%81%A8%E7%B4%8D%E7%A8%8E/?ev=40&lang=ja&s=4"
+  end
 
-url = "https://search.rakuten.co.jp/search/event/%E3%81%B5%E3%82%8B%E3%81%95%E3%81%A8%E7%B4%8D%E7%A8%8E/?ev=40&lang=ja&s=4"
+  def parse_html
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+    Nokogiri::HTML.parse(html, nil, charset)
+  end
 
-html = open(url) do |f|
-  charset = f.charset
-  f.read
+  def doc
+    @doc ||= parse_html
+  end
+
+  def category_names
+    @category_names ||= doc.css(".item > .dui-list ._ellipsis").map(&:text)
+  end
 end
-doc = Nokogiri::HTML.parse(html, nil, charset)
-
-category_names = doc.css(".item > .dui-list ._ellipsis").map(&:text)
