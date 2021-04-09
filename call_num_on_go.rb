e4b4444d8c4877b35c0754ffaccffcf07cbeb192
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-09 15:44:14 +0900

    need encode

--- call_num_on_go.rb
+++ call_num_on_go.rb
@@ -4,8 +4,8 @@ require 'open-uri'
 class CallNumOnGo
   attr_reader :url
 
-  def initialize(url)
-    @url = url
+  def initialize(word)
+    @url = "https://www.google.com/search?q=#{word}"
   end
 
   def parse_html
@@ -20,6 +20,10 @@ class CallNumOnGo
   def doc
     @doc ||= parse_html
   end
+
+  def num
+    doc.css(".Z1hOCe > div > span[2]")
+  end
 end
 
-p RakutenCategory.new("https://search.rakuten.co.jp/search/event/%E3%81%B5%E3%82%8B%E3%81%95%E3%81%A8%E7%B4%8D%E7%A8%8E/?ev=40&lang=ja&s=4").show
+p CallNumOnGo.new("MSK安心ステーション").url
