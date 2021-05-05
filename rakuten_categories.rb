7fe45a37953cea7f2cedfb7ab6fe49a1703700aa
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-05-05 12:10:01 +0900

    Modify: 引数を受け取れるようにする

--- rakuten_categories.rb
+++ rakuten_categories.rb
@@ -45,4 +45,4 @@ class RakutenCategory
   end
 end
 
-p RakutenCategory.new("https://search.rakuten.co.jp/search/event/%E3%81%B5%E3%82%8B%E3%81%95%E3%81%A8%E7%B4%8D%E7%A8%8E/?ev=40&lang=ja&s=4").show
+RakutenCategory.new(ARGV[0]).show
