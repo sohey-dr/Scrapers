695fe020075abf89a7057995155511e079c463d4
sohey-dr <heyso1209@gmail.com>
2021-06-22 01:10:11 +0900

    Add goquery code

---
+++ teikyo_syllabus/main.go
@@ -0,0 +1,25 @@
+package main
+ 
+import (
+	"fmt"
+	"log"
+	"net/http"
+ 
+	"github.com/PuerkitoBio/goquery"
+)
+ 
+const url = "https://www.e-campus.gr.jp/syllabus/kanri/hachioji/public/syllabus/2021"
+ 
+func main() {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+	defer res.Body.Close()
+ 
+	doc, _ := goquery.NewDocumentFromReader(res.Body)
+	doc.Find(".all-button > .btn").Each(func(i int, s *goquery.Selection) {
+		href, _ := s.Attr("href")
+		fmt.Println("https://www.e-campus.gr.jp" + href)
+	})
+}
