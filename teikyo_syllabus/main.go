5df944abc806fef9452b6d49489d199b41a1227d
sohey-dr <heyso1209@gmail.com>
2021-06-22 11:51:44 +0900

    Type declarative

--- teikyo_syllabus/main.go
+++ teikyo_syllabus/main.go
@@ -9,17 +9,18 @@ import (
 )
  
 const url = "https://www.e-campus.gr.jp/syllabus/kanri/hachioji/public/syllabus/2021"
- 
+
 func main() {
 	res, err := http.Get(url)
 	if err != nil {
 		log.Println(err)
 	}
 	defer res.Body.Close()
- 
+
 	doc, _ := goquery.NewDocumentFromReader(res.Body)
 	doc.Find(".all-button > .btn").Each(func(i int, s *goquery.Selection) {
 		href, _ := s.Attr("href")
-		fmt.Println("https://www.e-campus.gr.jp" + href)
+		var next_url string = "https://www.e-campus.gr.jp" + href
+		fmt.Println(next_url)
 	})
 }
