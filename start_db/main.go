67ed84e874260db1d45da3c2b21b3e77c4e542c8
soheyHashimoto <heyso1209@gmail.com>
2021-09-11 01:43:04 +0900

    [ADD]getComUrlByList

--- start_db/main.go
+++ start_db/main.go
@@ -9,7 +9,8 @@ import (
 )
 
 func main() {
-	getCatgoryUrls()
+	// getCatgoryUrls()
+	getComUrlByList()
 }
 
 func getCatgoryUrls()  {
@@ -21,6 +22,21 @@ func getCatgoryUrls()  {
 
 	doc, _ := goquery.NewDocumentFromReader(res.Body)
 	doc.Find(".tag-children-ul > li > a").Each(func(i int, s *goquery.Selection) {
+		href, _ := s.Attr("href")
+		var next_url string = `"https://startup-db.com` + href + `,"`
+		fmt.Println(next_url)
+	})
+}
+
+func getComUrlByList()  {
+	res, err := http.Get("https://startup-db.com/tags/cloud-funding")
+	if err != nil {
+		log.Println(err)
+	}
+	defer res.Body.Close()
+
+	doc, _ := goquery.NewDocumentFromReader(res.Body)
+	doc.Find(".CompanyCard > a").Each(func(i int, s *goquery.Selection) {
 		href, _ := s.Attr("href")
 		var next_url string = `"https://startup-db.com` + href + `,"`
 		fmt.Println(next_url)
